6a7881bb4a9bdf597cf9d80ac7da78ba281ed53f
martinv529 <mvaca@uade.edu.ar>
2025-04-26 14:22:12 -0300

    cambio opciones del menu y agrego una opcion mas

--- menu/menu.py
+++ menu/menu.py
@@ -17,11 +17,12 @@ def mostrar_menu():
     print(" ")
     print("1. Listado de todos los articulos y precio")
     print("2. Listado de todos los clientes")
-    print("3. Estadísticas de ventas por articulos")
-    print("4. Estadísticas de ventas por clientes")
-    print("5. Estadísticas de ventas por producto en específico ")
-    print("6. Estadísticas de ventas por cliente en específico ")
-    print("7. Salir")
+    print("3. Listado de todas las ventas")
+    print("4. Estadísticas de ventas por articulos")
+    print("5. Estadísticas de ventas por clientes")
+    print("6. Estadísticas de ventas por producto en específico ")
+    print("7. Estadísticas de ventas por cliente en específico ")
+    print("8. Salir")
 
 bandera = True
 while bandera:
@@ -99,7 +100,35 @@ while bandera:
                     print(" ")
                     print("Opción no válida. Intenta de nuevo.")
     elif eleccion == 3:
-        opcion_3()
+        while True:
+            print("1. Imprimir listado de ventas")
+            print("2. agregar venta/s")
+            print("3. Editar venta/s")
+            print("4. Eliminar venta/s")
+            print("5. Volver al menú principal")
+            
+            try:
+                entrada = input("Seleccione su opción (1-5) \n").strip()
+                if entrada == "":
+                    raise ValueError("Vacío")
+                opcion = int(entrada)
+            except ValueError:
+                print("Opción no válida. Intenta de nuevo.\n")
+                continue  # vuelve a mostrar el submenú
+
+            if opcion == 1:
+                sub_opcion2_1()
+            elif opcion == 2:
+                sub_opcion2_2()
+            elif opcion == 3:
+                sub_opcion2_3()
+            elif opcion == 4:
+                sub_opcion2_4()
+            elif opcion == 5:
+                break 
+            else: 
+                    print(" ")
+                    print("Opción no válida. Intenta de nuevo.")
     elif eleccion == 4:
         opcion_4()
     elif eleccion == 5:
@@ -107,6 +136,8 @@ while bandera:
     elif eleccion == 6:
         opcion_6()
     elif eleccion == 7:
+        opcion_6()
+    elif eleccion == 8:
         print("Saliste del programa.")
         bandera = False
     else:
