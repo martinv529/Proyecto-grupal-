b81cb34fe2655b264b76fcdc100526a9d000c57c
martinv529 <mvaca@uade.edu.ar>
2025-04-27 18:33:41 -0300

    agregamos validacion a las funciones y terminamos el tp

---
+++ Proyecto-grupal-/main.py
@@ -0,0 +1,17 @@
+
+
+""" Desarrollar una plataforma que lea datos de ventas desde archivos CSV y JSON. Utilizar ma
+    trices para representar datos de ventas, listas avanzadas y diccionarios para análisis y repor
+    tes. Implementar manejo de excepciones y recursividad para cálculos avanzados. Controlar 
+    el proyecto con Git y realizar pruebas unitarias para asegurar la calidad. """
+
+
+
+
+
+from funciones.menu import mostrar_menu
+
+
+
+
+mostrar_menu()
